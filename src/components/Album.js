2d2e635076288b444ad38765ed0f2b20411a1b73
sean sullivan <ssullivandev@gmail.com>
2018-11-11 21:17:49 -0600

    audio playback checkpoint complete

--- src/components/Album.js
+++ src/components/Album.js
@@ -1,18 +1,73 @@
 import React, { Component } from 'react';
 import albumData from './../data/albums';
 
+const itemStyle = {
+
+}
+
 class Album extends Component {
   constructor(props){
     super(props);
-  
     const album = albumData.find(album => {
       return album.slug === this.props.match.params.slug
     })
 
     this.state = {
-      album: album
+      album: album,
+      currentSong: album.songs[0],
+      isPlaying: false,
+      play: false,
+      hovered: album.songs[0]
     };
-  
+
+    this.toggleHoverPlayOn = this.toggleHoverPlayOn.bind(this)
+    this.toggleHoverPlayOff = this.toggleHoverPlayOn.bind(this)
+
+    this.audioElement = document.createElement('audio');
+    this.audioElement.src = album.songs[0].audioSrc;
+  }
+  toggleHoverPlayOn(song){
+    this.setState({play: true, hovered: song })
+    console.log('current hovered song: ', this.state.hovered)
+  }
+
+  toggleHoverPlayOff() {
+    this.setState({play: false})
+  }
+
+  play() {
+    this.audioElement.play();
+    this.setState({ isPlaying: true });
+  }
+
+  pause() {
+    this.audioElement.pause();
+    this.setState({ isPlaying: false });
+  }
+
+  setSong(song) {
+    this.audioElement.src = song.audioSrc;
+    this.setState({ currentSong: song });
+  }
+
+  handleSongClick(song) {
+    const isSameSong = this.state.currentSong === song;
+    if (this.state.isPlaying && isSameSong) {
+      this.pause();
+    } else {
+      if (!isSameSong) { this.setSong(song); } 
+      this.play();
+    }
+  }
+
+  isPlaying(song,index) {
+    if(this.state.currentSong === song && this.state.isPlaying){
+      return <span className="ion-md-pause"></span>;
+    } else if (this.state.hovered === song && this.state.play === true){
+      return <span className="ion-md-play"></span>;
+    } else {
+      return <span>{index + 1}</span>;
+    }
   }
 
   render(){
@@ -31,14 +86,15 @@ class Album extends Component {
              <col id="song-number-column" />
              <col id="song-title-column" />
              <col id="song-duration-column" />
-           </colgroup>  
+           </colgroup>
            <tbody>
               {
                 this.state.album.songs.map( (song, index) =>
-                <tr key={index}>
-                  <td>{index + 1}</td>
-                  <td>{song.title}</td>
-                  <td>{song.duration}</td>
+                <tr className="song" key={index} onClick={() => this.handleSongClick(song)} >
+                  <td onMouseEnter={() => this.toggleHoverPlayOn(song)} onMouseLeave={this.toggleHoverPlayOff}>
+                  {this.isPlaying(song,index)}</td>
+                  <td onMouseEnter={() => this.toggleHoverPlayOn(song)} onMouseLeave={this.toggleHoverPlayOff}>{song.title}</td>
+                  <td onMouseEnter={() => this.toggleHoverPlayOn(song)} onMouseLeave={this.toggleHoverPlayOff}>{song.duration}</td>
                 </tr>
                 )
               }
